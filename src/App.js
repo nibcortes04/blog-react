68b9b5c05cacd7db7b9e578824d7a6084b24c59e
MrDream <nicolas.b.cortes@gmail.com>
2022-08-19 12:17:07 -0500

    add basename in routes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Home } from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <Routes basename={process.env.PUBLIC_URL}>
         <Route exact path="/" element={<Login />} />
         <Route exact path="/Home" element={<Home />} />
       </Routes>
